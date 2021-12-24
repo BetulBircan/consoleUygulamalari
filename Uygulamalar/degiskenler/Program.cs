02d58e20328728580ac63db2b7a871ac03628cc6
Betul Bircan <bircan.betul_1996@hotmail.com>
2021-12-25 02:22:43 +0300

    değişken tipleri tanımlama

--- Uygulamalar/degiskenler/Program.cs
+++ Uygulamalar/degiskenler/Program.cs
@@ -6,7 +6,57 @@ namespace degiskenler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
+            byte b = 5;   //1 byte-0 ile 256
+            sbyte c = 5;  //1 byte -128---+127
+
+            short s = 5;    //2 byte
+            ushort us = 5;  //2 byte 0 ve pozitif sayılar alır
+
+            Int16 i16 = 2;  //2 byte
+            int i = 2;      //4 byte
+            Int32 i32 = 2;  //4 byte
+            Int64 i64 = 2;  //8 byte
+
+            uint ui = 2;    //4 byte  işaretsiz
+            long l = 4;     //8 byte  
+            ulong ul = 4;   //8 byte işaretsiz
+
+            //Reel Sayılar
+            float f = 5;        //4 byte
+            double d = 5;       //8 byte
+            decimal de = 5;     //16 byte
+
+            //Mantıksal değer
+            char ch = '2';          //2 byte
+            string str = "Betül";   //sınırsız
+
+            //Tarih
+            DateTime dt = DateTime.Now;
+            Console.WriteLine(dt);
+
+            object o1 = "xxx";
+            object o2 = 'y';
+            object o3 = 4;
+            object o4 = 12.5;
+
+            //string ifadeler
+            //ilk tanımlandığında içinde değer olmamasını istemiyorsak üç şekilde kullanılır:
+            //string str1 = "";     string str = null;
+            string str1 = string.Empty;
+            str1 = "Betül Bircan";
+            string ad = "Betül";
+            string soyad = "BİRCAN";
+            string tamIsim = ad + " " + soyad;
+            
+            //integer tanımlama şekilleri
+            int integer1 = 5;
+            int integer2 = 3;
+            int integer3 = integer1 * integer2;
+
+            //boolean
+            bool bo1 = 10<2;
+
         }
     }
 }
