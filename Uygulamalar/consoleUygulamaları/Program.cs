df98c021442b9d8e75b369c17248932482107be0
Betül Bircan <86554799+BetulBircan@users.noreply.github.com>
2021-12-24 17:39:38 +0300

    Add files via upload

---
+++ Uygulamalar/consoleUygulamaları/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace consoleUygulamaları
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi Giriniz: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı Giriniz: ");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba " + name + " " +surname);
+        }
+    }
+}
