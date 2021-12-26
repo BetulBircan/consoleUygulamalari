8ad8a6e3abe39c93f718a1eef093b54599c29b9c
Betul Bircan <bircan.betul_1996@hotmail.com>
2021-12-26 03:37:14 +0300

    atama ve işlemli atama operatörleri kodları eklendi

--- Uygulamalar/operatorler/Program.cs
+++ Uygulamalar/operatorler/Program.cs
@@ -6,7 +6,19 @@ namespace operatorler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***********Atama ve İşlemli Atama Operatörleri**********");
+            //Atama ve İşlemli Atama
+            int x = 3;
+            int y = 3;
+            
+            y = y+2;
+            Console.WriteLine("y = y+2 : "+y);
+            y += 2;  //işlemli atama olur.
+            Console.WriteLine("y += 2 : "+y);
+            y /= 1; 
+            Console.WriteLine("y /= 1 : "+y);
+            x *= 2;
+            Console.WriteLine("x *= 2 : "+x);
         }
     }
 }
