11b67749d34abe487fe48a22139638932a41c98d
Betul Bircan <bircan.betul_1996@hotmail.com>
2021-12-27 02:43:14 +0300

    tip dönüşümlerinden implicir conversion kodları eklendi.

--- Uygulamalar/tipDonusumleri/Program.cs
+++ Uygulamalar/tipDonusumleri/Program.cs
@@ -6,7 +6,26 @@ namespace tipDonusumleri
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Tip Dönüşümleri
+           //İmplicit Conversion (Bilinçsiz Dönüşüm): Düşük kapasiteli bir değişkeni kendinden daha yüksek kapasiteli başka bir değişkene atanma işlemi.sistem kendisi otomotik dönüştürür bizim parse convert diye mmetod yazmamıza gerek kalmaz.
+           Console.WriteLine("*******İmplicit Conversion*******");
+           byte a = 5;
+           sbyte b = 30;
+           short c = 10;
+           
+           int d = a+b+c;
+           Console.WriteLine("d : "+d);
+
+           long h = d;
+           Console.WriteLine("h : "+h);
+
+           float i = h;
+           Console.WriteLine("i : "+i);
+
+           string e = "Betül";
+           char f = 'k';
+           object g = e+f+d;
+           Console.WriteLine("g : "+g);  //bütün veri tipleri objectten türemiştir.Bu yüzden object hepsinden üstündedir.
         }
     }
 }
