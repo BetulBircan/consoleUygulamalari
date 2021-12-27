73d94caed614675bffe99e1666083ec83fc8d79b
Betul Bircan <bircan.betul_1996@hotmail.com>
2021-12-28 01:56:33 +0300

    hataYonetimi adlı proje dosyası oluşturuldu.

---
+++ Uygulamalar/hataYonetimi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hataYonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
