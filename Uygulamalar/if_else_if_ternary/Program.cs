89407e852400c15156534f9d250e58c18d9e7180
Betul Bircan <bircan.betul_1996@hotmail.com>
2021-12-29 02:00:48 +0300

    if elseif yapısı ile ilgili örnek kodlar

--- Uygulamalar/if_else_if_ternary/Program.cs
+++ Uygulamalar/if_else_if_ternary/Program.cs
@@ -6,7 +6,16 @@ namespace if_else_if_ternary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             //İf-else yapısı
+            //şimdiki saati alıp gündüz mü gece mi kontrol eden programı yazma
+            int time = DateTime.Now.Hour;  //saati alma
+
+            if(time >=6 && time < 11)
+                Console.WriteLine("Günaydın!");
+            else if(time<=18)  //Koşul sağlanırsa çalışacağı blok.Tek satır olduğu için {} blokları yazmamıza gerek yok.
+                Console.WriteLine("İyi Günler!");
+            else  //koşul sağlanmadığı zaman çalışacağı bloktur.
+                Console.WriteLine("İyi Geceler!");
         }
     }
 }
