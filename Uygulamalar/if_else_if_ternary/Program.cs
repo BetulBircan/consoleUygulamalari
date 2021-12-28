da2b05e1f050ca99a10aa567d263c64ec3955ac3
Betul Bircan <bircan.betul_1996@hotmail.com>
2021-12-29 01:55:16 +0300

    if elseif ternary proje dosyası oluşturuldu.

---
+++ Uygulamalar/if_else_if_ternary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace if_else_if_ternary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
