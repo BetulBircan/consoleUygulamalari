30a6c0e3113ed74688be6b9899409bf044170a7f
Betul Bircan <bircan.betul_1996@hotmail.com>
2021-12-30 01:49:40 +0300

    daha sonra hangi ayda olduğumuzu kontrol etmesi için yazılan case kodları.

--- Uygulamalar/switch_case/Program.cs
+++ Uygulamalar/switch_case/Program.cs
@@ -25,8 +25,44 @@ namespace switch_case
 
             switch (month)
             {
+                case 1:
+                    Console.WriteLine("Ocak Ayındasınız.");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat Ayındasınız.");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart Ayındasınız.");
+                    break;
+                case 4:
+                    Console.WriteLine("Nisan Ayındasınız.");
+                    break;
+                case 5:
+                    Console.WriteLine("Mayıs Ayındasınız.");
+                    break;
+                case 6:
+                    Console.WriteLine("Haziran Ayındasınız.");
+                    break;
+                case 7:
+                    Console.WriteLine("Temmuz Ayındasınız.");
+                    break;
+                case 8:
+                    Console.WriteLine("Ağustos Ayındasınız.");
+                    break;
+                case 9:
+                    Console.WriteLine("Eylül Ayındasınız.");
+                    break;
+                case 10:
+                    Console.WriteLine("Ekim Ayındasınız.");
+                    break;
+                case 11:
+                    Console.WriteLine("Kasım Ayındasınız.");
+                    break;
+                case 12:
+                    Console.WriteLine("Aralık Ayındasınız.");
+                    break;
                 
-                default:
+                default:  // hiçbir case e uymadığı takdirde yapılacak işlemi ifade eder.break  ifadesi ile de çıkış yapılabilir.default kısmı olmak zorundadır.
                     break;
             }
         }
