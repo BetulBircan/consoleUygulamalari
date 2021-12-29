395cde91d10867b2c99a334ee5cee02e88e5346d
Betul Bircan <bircan.betul_1996@hotmail.com>
2021-12-30 01:55:32 +0300

    diğer aylarınhangi mevsime denk geldiğini gösteren diğer kod kısmı yapıldı.

--- Uygulamalar/switch_case/Program.cs
+++ Uygulamalar/switch_case/Program.cs
@@ -83,6 +83,21 @@ namespace switch_case
                 case 2:
                     Console.WriteLine("Kış Mevsimindesiniz.");
                     break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar Mevsimindesiniz.");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz Mevsimindesiniz");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar Mevsimindesiniz.");
+                    break;
                 
                 default:
                     break;
