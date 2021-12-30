ff7b070a472b272807acaee0894424c0f02f4ef1
Betul Bircan <bircan.betul_1996@hotmail.com>
2021-12-30 23:43:25 +0300

    donguler_forloop proje dosyası oluşturuldu.

---
+++ Uygulamalar/donguler_forloop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace donguler_forloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
