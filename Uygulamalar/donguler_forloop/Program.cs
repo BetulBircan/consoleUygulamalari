1a574ebce1ff23912713332ab4c4a1ffb85646fa
Betul Bircan <bircan.betul_1996@hotmail.com>
2021-12-30 23:46:37 +0300

    for döngüsü tanıtımı yapıldı.

--- Uygulamalar/donguler_forloop/Program.cs
+++ Uygulamalar/donguler_forloop/Program.cs
@@ -6,7 +6,11 @@ namespace donguler_forloop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Döngüler
+            //Belli bir kod bloğunun belli şartlar altında,şartlara bağlı kalarak birden fazla defa çalışmasını istiyorsak for loop(döngü) kullanılır.
+            //for(başlangıç noktası;koşul;artım)
+
+            //Ekrandan girilen sayıya kadar olan tek sayıları ekrana yazdır.
         }
     }
 }
