05cbf58610915cb10a612ec3ac7b8fa43d2bd998
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-01-04 22:30:00 +0300

    sort metodunu göstermek amacıyla ilk başta sırasız dizi tanımlandı.

--- Uygulamalar/diziler_ArraySinifiMetodlari/Program.cs
+++ Uygulamalar/diziler_ArraySinifiMetodlari/Program.cs
@@ -6,7 +6,14 @@ namespace diziler_ArraySinifiMetodlari
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Diziler System.Array sınıfından türerler. Array sınıfı da dizilerle yapılan işlemleri kolaylaştırmak için bir takım static hazır metotlar sunar.Sık kullanılan Array metotları:
+
+            //Sort:Diziler üzerinden sıralama işlemi yapar. Eğer string bir dizi ise alfabetik olarak olarak A'dan Z'ye sıralar. Eğer numeric bir dizi ise dizi elemanlarını küçükten büyüğe sıralar.
+            
+            int[] sayiDizisi = {23, 12, 4, 86, 72, 3, 11, 17};
+            Console.WriteLine("******Sırasız Dizi******");
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
         }
     }
 }
